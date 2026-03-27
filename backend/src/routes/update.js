918a69d3626c41568d1ce625ae3bd22ce15fbf09
MEmanuel1986 <markus.emanuel@gmail.com>
2026-03-27 06:06:01 +0100

    cleanup: Duplikate, Altlasten und Legacy entfernt

--- backend/src/routes/update.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const ctrl    = require('../controllers/updateController');
-const router  = express.Router();
-
-router.post('/preview', ctrl.preview);
-router.post('/apply',   ctrl.apply);
-
-module.exports = router;
